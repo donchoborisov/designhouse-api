b14d3d923febd99f474acfadbe6b14fdf44c67f0
Doncho Borisov <donco_borisov@yahoo.com>
2020-11-18 22:43:27 +0000

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+//public routes
+Route::get('me','User\MeController@getMe');
+
+
+
+//Routes for authenticated users only
+Route::group(['middleware'=>['auth:api']],function(){
+   Route::post('logout','Auth\LoginController@logout');
+   Route::put('settings/profile','User\SettingsController@updateProfile');
+   Route::put('settings/password','User\SettingsController@updatePassword');
+
+
+});
+
+//Routes for guests only
+Route::group(['middleware'=>['guest:api']],function(){
+   Route::post('register','Auth\RegisterController@register');
+   Route::post('verification/verify/{user}','Auth\VerificationController@verify')->name('verification.verify');
+   Route::post('verification/resend','Auth\VerificationController@resend');
+   Route::post('login','Auth\LoginController@login');
+   Route::post('password/email','Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.reset');
+   Route::post('password/reset','Auth\ResetPasswordController@reset');
+
+ 
+
+});
+
